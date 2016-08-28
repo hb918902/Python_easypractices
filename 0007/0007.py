e493f3944aaa866f034b6cd14e5048678c467982
hb <houbo_1020@163.com>
2016-08-28 21:26:03 +0800

    modify 0007

--- 0007/0007.py
+++ 0007/0007.py
@@ -1,7 +1,6 @@
 #! usr/bin/env python3
 # coding = utf-8
 
-"""仅适用于当前目录中的py文件，多层目录无法计算"""
 
 import os
 import re
@@ -23,25 +22,38 @@ def count_code(filename):
             else:
                 code_cnt += 1
             sum_cnt += 1
-    print("{0} 有{1}行，其中{2}代码，{3}行注释，{4}行空白行。".format(filename,sum_cnt,code_cnt,note_cnt,blank_cnt))
+    dictcnt = {'sum':sum_cnt,'note':note_cnt,'blank':blank_cnt,'code':code_cnt}
+    return dictcnt
 
-def get_dir(path):
-    files = os.listdir(path)
-    for file in files:
-        if file.endswith('.py'):
-            count_code(file)
+
+def get_recursive_file(path):
+    current_files = os.listdir(path)
+    files = []
+    for file in current_files:
+        full_file = os.path.join(path,file)
+        files.append(full_file)
+        if os.path.isdir(full_file):
+            next_level_files = get_recursive_file(full_file)
+            files.extend(next_level_files)
+    return files
 
 if __name__ == "__main__":
-    get_dir('.')
+    sum_cnt = 0
+    note_cnt = 0
+    blank_cnt = 0
+    code_cnt = 0
+    for file in get_recursive_file('.'):
 
-
-
-
-
-
-
-
-
-
-
-
+        if file.endswith('.py'):
+            count_code(file)
+            sum_cnt += count_code(file)['sum']
+            note_cnt += count_code(file)['note']
+            blank_cnt += count_code(file)['blank']
+            code_cnt += count_code(file)['code']
+            print("{0}有{1}行,其中{2}行代码，{3}行注释，{4}行空白行".format(
+                file,
+                count_code(file)['sum'],
+                count_code(file)['code'],
+                count_code(file)['note'],
+                count_code(file)['blank']))
+    print("当前目录有{0}行,其中{1}行代码，{2}行注释，{3}行空白行".format(sum_cnt,code_cnt,note_cnt,blank_cnt))
